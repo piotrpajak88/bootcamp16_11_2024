1f9af14c9859837b58e31bafa3bf8b283e3f2262
piotrpajak88 <88ironspider88@gmail.com>
2024-11-16 13:54:10 +0100

    pierwszy.py

--- Day1_16_11/pierwszy.py
+++ Day1_16_11/pierwszy.py
@@ -1,1 +1,18 @@
+import sys
 print()
+print(sys.int_info)
+
+
+name = "Radek"
+print(type(name))
+print(name)
+
+name_info: str = 'Maciek'
+print(type(name_info))
+print(name_info)
+
+
+name_info = 58
+print(name_info)
+print(type(name_info))
+
